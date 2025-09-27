92df7c262e21312c74b2420a4f19322204afc889
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2025-09-26 18:04:52 -0700

    add support for resolver.MultiResolver; update vendor deps (#3)
    Co-authored-by: thisisaaronland <thisisaaronland@localhost>

--- reader_test.go
+++ reader_test.go
@@ -84,3 +84,36 @@ func TestHTTPFindingAid(t *testing.T) {
 		t.Fatalf("Expected %s to exists", uri)
 	}
 }
+
+func TestMultiFindingAid(t *testing.T) {
+
+	ctx := context.Background()
+
+	reader_uri := "findingaid://multi?resolver=https%3A%2F%2Fstatic.sfomuseum.org%2Ffindingaid%3Ftemplate%3Dhttps%3A%2F%2Fraw.githubusercontent.com%2Fsfomuseum-data%2F%7Brepo%7D%2Fmain%2Fdata%2F&resolver=https%3A%2F%2Fdata.whosonfirst.org%2Ffindingaid%3Ftemplate%3Dhttps%3A%2F%2Fraw.githubusercontent.com%2Fwhosonfirst-data%2F%7Brepo%7D%2Fmaster%2Fdata%2F"
+
+	r, err := reader.NewReader(ctx, reader_uri)
+
+	if err != nil {
+		t.Fatalf("Failed to create new reader, %v", err)
+	}
+
+	uri := "85865975"
+
+	fh, err := r.Read(ctx, uri)
+
+	if err != nil {
+		t.Fatalf("Failed to read %s, %v", uri, err)
+	}
+
+	fh.Close()
+
+	exists, err := r.Exists(ctx, uri)
+
+	if err != nil {
+		t.Fatalf("Failed to determine if %s exists, %v", uri, err)
+	}
+
+	if !exists {
+		t.Fatalf("Expected %s to exists", uri)
+	}
+}
